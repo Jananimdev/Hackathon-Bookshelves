9233459ba79cbc78d407966194622b7b54247222
Jananimdev <jananimayilarasu@gmail.com>
2025-05-28 22:25:44 +0530

    changed the beforeAll to beforeEach for safe run in parallel

--- tests/Home.spec.js
+++ tests/Home.spec.js
@@ -4,29 +4,21 @@ const Products = require("../Pages/Products");
 const ProductDetail = require("../Pages/ProductDetail");
 const input = require("../data/input.json");
 
-let page;
-let home;
-test.beforeAll(async ({ browser }) => {
-  let context = await browser.newContext();
-  page = await context.newPage();
-  home = new Home(page);
+test.beforeEach(async ({ page }) => {
+  const home = new Home(page);
+  await home.goTo();
 });
-test.afterAll(async () => {
-  await page.close();
-});
-test.beforeEach(async () => {
-  await test.step("Navigate to the webpage", async () => {
-    await home.goTo();
-  });
-});
-test("Bookshelves", async () => {
+
+test("Bookshelves", async ({ page }) => {
+  const home = new Home(page);
   const product = new Products(page);
+
   await test.step("Click BookShelves", async () => {
-    //reusability of the code with differnt inputs
     await home.clickProduct(input.searchProduct);
     await product.closePopup();
-    await page.waitForTimeout(2000)
+    await page.waitForTimeout(2000);
   });
+
   await test.step("Apply filters", async () => {
     await product.filterPriceRange();
     await page.waitForTimeout(2000);
@@ -35,29 +27,35 @@ test("Bookshelves", async () => {
     await product.clickOutOfStock();
     await page.waitForTimeout(2000);
   });
-  await test.step("Get BookShelves Deatils", async () => {
+
+  await test.step("Get BookShelves Details", async () => {
     await product.getProducts();
-    await page.screenshot({path:'screenshot/products.png'});
+    await page.screenshot({ path: 'screenshot/products.png' });
   });
 });
 
+test("Display collections", async ({ page }) => {
+  const home = new Home(page);
 
-test("Display collections", async () => {
   await test.step("Get Collection List", async () => {
     await home.hoverCollection();
     await home.getListItems();
-    await page.screenshot({path:'screenshot/collections.png'});
+    await page.screenshot({ path: 'screenshot/collections.png' });
   });
 });
 
-test("Giftcards", async () => {
+test("Giftcards", async ({ page, context }) => {
+  const home = new Home(page);
   const products = new Products(page);
+
   let product;
   let page2;
+
   await test.step("Searching the product", async () => {
     await home.fillInput(input.searchInput);
     await home.clickSearchButton();
   });
+
   await test.step("Click Product and checkout", async () => {
     page2 = await products.clickProduct();
     await page2.waitForLoadState();
@@ -66,10 +64,11 @@ test("Giftcards", async () => {
     await product.clickCheckOut();
     await page2.waitForTimeout(3000);
   });
+
   await test.step("Enter Invalid email credentials", async () => {
     await product.enterEmail(input.invalidEmail);
     await page2.keyboard.press("Tab");
     await product.getEmailError();
-    await page.screenshot({path:'screenshot/error.png'});
+    await page.screenshot({ path: 'screenshot/error.png' });
   });
 });
